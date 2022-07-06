00213603acc450e7bcd238752bfbd1511e5d78c3
Ananth <106952901+Ananthm032@users.noreply.github.com>
2022-07-06 17:44:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
 import { StyledEngineProvider } from "@mui/material"
-
+import { BrowserRouter } from 'react-router-dom';
 ReactDOM.render((
 <StyledEngineProvider>
-  <App/>
+ <BrowserRouter>
+      <App />
+      </BrowserRouter>
 </StyledEngineProvider>),document.getElementById('root'));
