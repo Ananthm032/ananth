d86db998c8391f428e1ebb82117fbb2815bc2c98
Ananth <106952901+Ananthm032@users.noreply.github.com>
2022-07-06 11:36:45 +0530

    Create Root.js

---
+++ src/route/Root.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Signin from "../component/Signin"
+
+export default function Signin(){
+    return(
+        <div>
+            <Signin/>
+        </div>
+    )
+}
